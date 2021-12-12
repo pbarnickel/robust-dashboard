2cf465d920942a83bfdca1e857703bb906fbc94d
Philipp Barnickel <philippbarnickel@gmail.com>
2021-12-12 05:05:48 +0100

    bugfix update script RBT burn history

--- php/updateDashboard.php
+++ php/updateDashboard.php
@@ -2,13 +2,13 @@
 
 use BPS\RobustDashboard\Main;
 
-require_once('php/Constants.php');
-require_once('php/Model/RobustBurnHistoryEntry.php');
-require_once('php/Model/RobustBurnHistory.php');
-require_once('php/Main.php');
-require_once('php/API/BaseAPI.php');
-require_once('php/API/RequestAPI.php');
-require_once('php/API/DatabaseAPI.php');
+require_once('Constants.php');
+require_once('Model/RobustBurnHistoryEntry.php');
+require_once('Model/RobustBurnHistory.php');
+require_once('Main.php');
+require_once('API/BaseAPI.php');
+require_once('API/RequestAPI.php');
+require_once('API/DatabaseAPI.php');
 
 $oMain = new Main();
 $oMain->runDailyScript();
