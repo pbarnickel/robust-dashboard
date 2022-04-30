011b224f5ebeff6992ab470b345fb2dd8b6c8122
Philipp Barnickel <philippbarnickel@gmail.com>
2022-04-30 18:32:23 +0200

    design modified

--- php/Model/RobustBurnHistory.php
+++ php/Model/RobustBurnHistory.php
@@ -60,19 +60,23 @@ class RobustBurnHistory implements Constants
     public function getHTML(RobustBurnHistoryEntry $oCurrentSituation){
 
         //table and header row
-        $sOutput = '<table id="bpsRbtMainTable" class="table"><thead><tr class="table-dark"><th scope="col">Date</th>';
-        $sOutput = $sOutput . '<th scope="col">RBT Burned Total</th><th scope="col">RBT Burned at Day</th>';
-        $sOutput = $sOutput . '<th scope="col">RBT Current Supply</th><th scope="col">RBT Available Supply</th></tr></thead><tbody>';
+        $sOutput = '<table id="bpsRbtMainTable" class="table"><thead><tr class="table-dark">';
+        $sOutput = $sOutput . '<th scope="col" class="align-middle">Date</th>';
+        $sOutput = $sOutput . '<th scope="col" class="align-middle">RBT Burned Total</th>';
+        $sOutput = $sOutput . '<th scope="col" class="align-middle">RBT Burned at Day</th>';
+        $sOutput = $sOutput . '<th scope="col" class="align-middle">RBT Current Supply</th>';
+        $sOutput = $sOutput . '<th scope="col" class="align-middle">RBT Available Supply</th>';
+        $sOutput = $sOutput . '</tr></thead><tbody>';
         
         //data today
         $sCurrentSupply = Constants::RBT_INIT_TOTAL_SUPPLY - $oCurrentSituation->getTotalBurned();
         $sAvailableSupply = $sCurrentSupply - Constants::RBT_LOCKED_SUPPLY;
         $oDate = date_create($oCurrentSituation->getDate());
         $sOutput = $sOutput . '<tr class="table-primary"><th scope="row">' . date_format($oDate,"Y/m/d") . '</th>';
-        $sOutput = $sOutput . '<td>' . number_format($oCurrentSituation->getTotalBurned(),2,".",",") . '</td>';
-        $sOutput = $sOutput . '<td>' . number_format($oCurrentSituation->getDifferenceBurned(),2,".",",") . '</td>';
-        $sOutput = $sOutput . '<td>'. number_format($sCurrentSupply,2,".",",");
-        $sOutput = $sOutput . '<td>' . number_format($sAvailableSupply,2,".",",") . '</td></tr>';
+        $sOutput = $sOutput . '<td class="align-middle">' . number_format($oCurrentSituation->getTotalBurned(),2,".",",") . '</td>';
+        $sOutput = $sOutput . '<td class="align-middle">' . number_format($oCurrentSituation->getDifferenceBurned(),2,".",",") . '</td>';
+        $sOutput = $sOutput . '<td class="align-middle">'. number_format($sCurrentSupply,2,".",",");
+        $sOutput = $sOutput . '<td class="align-middle">' . number_format($sAvailableSupply,2,".",",") . '</td></tr>';
 
         //historic data
         $iLen = $this->getSize();
@@ -80,12 +84,12 @@ class RobustBurnHistory implements Constants
             $oEntry = $this->aEntries[$i];
             $oDate = date_create($oEntry->getDate());
             $sOutput = $sOutput . '<tr><th scope="row">' . date_format($oDate,"Y/m/d") . '</th>';
-            $sOutput = $sOutput . '<td>' . number_format($oEntry->getTotalBurned(),2,".",",") . '</td>';
-            $sOutput = $sOutput . '<td>' . number_format($oEntry->getDifferenceBurned(),2,".",",") . '</td>';
+            $sOutput = $sOutput . '<td class="align-middle">' . number_format($oEntry->getTotalBurned(),2,".",",") . '</td>';
+            $sOutput = $sOutput . '<td class="align-middle">' . number_format($oEntry->getDifferenceBurned(),2,".",",") . '</td>';
             $sCurrentSupply = Constants::RBT_INIT_TOTAL_SUPPLY - $oEntry->getTotalBurned();
-            $sOutput = $sOutput . '<td>' . number_format($sCurrentSupply,2,".",",") . '</td>';
+            $sOutput = $sOutput . '<td class="align-middle">' . number_format($sCurrentSupply,2,".",",") . '</td>';
             $sAvailableSupply = $sCurrentSupply - Constants::RBT_LOCKED_SUPPLY;
-            $sOutput = $sOutput . '<td>' . number_format($sAvailableSupply,2,".",",") . '</td></tr>';
+            $sOutput = $sOutput . '<td class="align-middle">' . number_format($sAvailableSupply,2,".",",") . '</td></tr>';
         }
 
         $sOutput = $sOutput . '</tbody></table>';
