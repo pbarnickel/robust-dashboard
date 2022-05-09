477f2af19677de46afea4e0bc234bd153c5071a5
Philipp Barnickel <philippbarnickel@gmail.com>
2022-05-09 05:56:24 +0200

    scripts modified

--- php/scripts/updateData.php
+++ php/scripts/updateData.php
@@ -2,7 +2,17 @@
 
 use BPS\RobustDashboard\Main;
 
-require_once('require.php');
+require_once('../Constants.php');
+require_once('../Model/Entry.php');
+require_once('../Model/EntryRBT.php');
+require_once('../Model/EntryRBS.php');
+require_once('../Model/History.php');
+require_once('../Model/HistoryRBT.php');
+require_once('../Model/HistoryRBS.php');
+require_once('../API/API.php');
+require_once('../API/DatabaseAPI.php');
+require_once('../API/RobustAPI.php');
+require_once('../Main.php');
 
 $main = new Main();
 $main->updateDataRBT();
